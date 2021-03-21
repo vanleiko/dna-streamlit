fe1795b01528c85be91bfcbab9667be92c499518
vanleiko <vanleiko25@gmail.com>
2021-03-21 11:20:43 -0300

    add app-dna.py

---
+++ src/app-dna.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplitlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.title("Contagem de Nucleotídeos e Conteúdo GC do DNA")
+st.subheader("Web App que conta a quantidade de A, C, G, T e conteúdo GC de uma sequência de DNA")
+
+
