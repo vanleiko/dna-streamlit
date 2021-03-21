1b5701191a4c31692e739e8d54fc04d538d2704d
vanleiko <vanleiko25@gmail.com>
2021-03-21 15:32:27 -0300

    update app-dna.py

--- src/app-dna.py
+++ src/app-dna.py
@@ -2,9 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from PIL import Image
 
+imagem = Image.open("dna-logo.jpg")
+st.image(imagem, use_column_width=True)
 
-def formata_sequencia(sequencia):    
+def formata_sequencia(sequencia): 
+    sequencia = sequencia.upper()
+
     if sequencia[0] == ">":
         sequencia = sequencia.splitlines()
         sequencia = sequencia[1:]
@@ -17,33 +22,50 @@ def formata_sequencia(sequencia):
     return sequencia
 
 
-def conta_nucleotidos(sequencia):    
-    sequencia = sequencia.upper()
+def eh_dna(sequencia):
+    if set(sequencia).issubset({"A", "C", "G", "T"}):
+        return True
+    else:
+        return False
+
+
+def conta_nucleotideos(sequencia, selecao):    
+ 
+    total = len(sequencia)
 
     adenina = sequencia.count("A")
     citosina = sequencia.count("C")
     guanina = sequencia.count("G")
     timina = sequencia.count("T")
+    total = len(sequencia)
 
-    df = pd.DataFrame({"Quantidade": [adenina, citosina, guanina, timina]},
-                        index=["Adenina", "Citosina", "Guanina", "Timina"])
+    porc_adenina = (adenina / total) * 100
+    porc_citosina = (citosina / total) * 100
+    porc_guanina = (guanina / total) * 100
+    porc_timina = (timina / total) * 100
+    porc_total = porc_adenina + porc_citosina + porc_guanina + porc_timina
 
+    df = pd.DataFrame({"Quantidade": [adenina, citosina, guanina, timina, total],
+                        "Porcentagem (%)": [porc_adenina, porc_citosina, porc_guanina, porc_timina, porc_total]},
+                        index=["Adenina", "Citosina", "Guanina", "Timina", "Total"])
+      
     fig, ax = plt.subplots(figsize=(6,3))
-    ax = sns.barplot(x=df.index, y="Quantidade", data=df)
-    ax.set_title(f"Contagem de nucleotídeos", fontweight="bold", fontsize=14)
-    ax.set_ylabel("Quantidade", fontsize=12)
+    ax = sns.barplot(x=df.index[:4], y=selecao, data=df.iloc[:4])
+    ax.set_title(f"{selecao} de nucleotídeos", fontweight="bold", fontsize=14)
+    ax.set_ylabel(f"{selecao}", fontsize=12)
+    if selecao == "Porcentagem (%)":
+        ax.set_ylim(0, 100)
   
     return df, fig
 
 
 def conteudo_gc(sequencia):
-    sequencia = sequencia.upper()
-
+    
     gc = sequencia.count("G") + sequencia.count("C")
-    porcentagem_gc = (gc / len(sequencia)) * 100
-    porcentagem_at = 100 - porcentagem_gc
+    porc_gc = (gc / len(sequencia)) * 100
+    porc_at = 100 - porc_gc
      
-    df = pd.DataFrame({"Porcentagem (%)": [porcentagem_gc, porcentagem_at]},
+    df = pd.DataFrame({"Porcentagem (%)": [porc_gc, porc_at]},
                         index=["GC", "AT"])
     
     fig, ax = plt.subplots(figsize=(6,3))
@@ -57,31 +79,34 @@ def conteudo_gc(sequencia):
 
 def main():
     
-    st.title("Contagem de Nucleotídeos e Conteúdo GC do DNA")
-    st.markdown("Web App que conta a quantidade de **A C G T** e o **Conteúdo GC** de uma sequência de **DNA**.")
+    st.title("# Contando nucleotídeos no DNA")
+    st.markdown("Web App que conta a quantidade e porcentagem de **A C G T** e o **Conteúdo GC** de uma sequência de **DNA**.")
 
-    st.subheader("**Insira sua sequência de DNA:**")
-    seq_input = """>Meu_DNA
-                    GAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTGCTCGG
-                    ATCTTCCTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATG
-                    TGAACCCATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCC"""
-    sequencia = st.text_area("Insira uma sequência de nucleótideos ou formato FASTA", seq_input, height=300)
-    sequencia_formatada = formata_sequencia(sequencia)
-    st.write("Sequência inserida:", sequencia_formatada)
-       
-    st.subheader("**1. Quantidade de nucleotídeos:**")
-    df_nucleotideo, grafico_nucleotideo = conta_nucleotidos(sequencia_formatada) 
-    st.dataframe(df_nucleotideo)
-    st.pyplot(grafico_nucleotideo)
+    st.subheader("**Insira a sua sequência de DNA**")
+    help_texto = "Insira uma sequência de nucleotídeos ou uma sequência no formato FASTA"
+    sequencia = st.text_area(label="Insira abaixo a sua sequência e pressione Ctrl + ENTER", height=250, help=help_texto)
 
-    st.subheader("**2. Conteúdo GC:**")
-    df_gc, grafico_gc = conteudo_gc(sequencia_formatada) 
-    st.dataframe(df_gc)
-    st.pyplot(grafico_gc)
+    if sequencia:
+        sequencia_formatada = formata_sequencia(sequencia)
+        
+        if eh_dna(sequencia_formatada):
+  
+            st.subheader("**# 1. Análise dos nucleotídeos:**")
+            selecao = st.selectbox("Selecione o gráfico:", ["Quantidade", "Porcentagem (%)"])
+            df_nucleotideo, grafico_nucleotideo = conta_nucleotideos(sequencia_formatada, selecao) 
+            st.dataframe(df_nucleotideo)
+            st.pyplot(grafico_nucleotideo)
+
+            st.subheader("**# 2. Conteúdo GC:**")
+            df_gc, grafico_gc = conteudo_gc(sequencia_formatada) 
+            st.dataframe(df_gc)
+            st.pyplot(grafico_gc)
+
+        else:
+            st.write("*A sequência inserida não é um DNA =(*")
+            st.write("*Insira uma sequência de DNA*")
 
 
 if __name__ == "__main__":
     main()
-
-
 
