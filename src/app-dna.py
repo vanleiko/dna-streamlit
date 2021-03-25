725874897b8a1233909e322708b8c708ac5d07ca
vanleiko <vanleiko25@gmail.com>
2021-03-25 15:22:24 -0300

    added needleman-wunsch code

--- src/app-dna.py
+++ src/app-dna.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 from collections import Counter
 import streamlit as st
@@ -9,117 +10,275 @@ from PIL import Image
 @st.cache
 def formata_sequencia(sequencia): 
     sequencia = sequencia.upper()
-
+        
     if sequencia[0] == ">":
         sequencia = sequencia.splitlines()
         sequencia = sequencia[1:]
         sequencia = "".join(sequencia)
-    
+        sequencia = "*" + sequencia
+        
     else:
         sequencia = sequencia.splitlines()
         sequencia = "".join(sequencia)
-    
+        sequencia = "*" + sequencia 
+        
     return sequencia
 
 
-def eh_dna(sequencia):
-    if set(sequencia).issubset({"A", "C", "G", "T"}):
+def eh_dna(seq):
+    if set(seq).issubset({"A", "C", "G", "T", "*"}):
         return True
     else:
         return False
 
 
-def calcula_porcentagem(nucleotideo, total):
-    porcentagem = (nucleotideo / total) * 100
+def conta_nucleotideo(seq):
     
-    return porcentagem
+    mapa = Counter(seq)
 
+    adenina = mapa["A"]
+    citosina = mapa["C"]
+    guanina = mapa["G"]
+    timina = mapa["T"]
+    total = len(seq)-1
+    quantidade = [adenina, citosina, guanina, timina, total]
 
-def conta_nucleotideos(sequencia, selecao="Quantidade"): 
-    conta = Counter(sequencia) 
-    total = len(sequencia)
- 
-    adenina = conta["A"]
-    citosina = conta["C"]
-    guanina = conta["G"]
-    timina = conta["T"] 
- 
-    porc_adenina = calcula_porcentagem(adenina, total)
-    porc_citosina = calcula_porcentagem(citosina, total)
-    porc_guanina = calcula_porcentagem(guanina, total)
-    porc_timina = calcula_porcentagem(timina, total)
+    porc_adenina = (adenina / total) * 100
+    porc_citosina = (citosina / total) * 100
+    porc_guanina = (guanina / total) * 100
+    porc_timina = (timina / total) * 100
     porc_total = porc_adenina + porc_citosina + porc_guanina + porc_timina
+    porcentagem = [porc_adenina, porc_citosina, porc_guanina, porc_timina, porc_total]
 
-    df = pd.DataFrame({"Quantidade": [adenina, citosina, guanina, timina, total],
-                        "Porcentagem (%)": [porc_adenina, porc_citosina, porc_guanina, porc_timina, porc_total]},
-                        index=["Adenina", "Citosina", "Guanina", "Timina", "Total"])
-      
-    fig, ax = plt.subplots(figsize=(6,3))
-    ax = sns.barplot(x=df.index[:4], y=selecao, data=df.iloc[:4])
-    ax.set_title(f"{selecao} de nucleotídeos", fontweight="bold", fontsize=14)
-    ax.set_ylabel(f"{selecao}", fontsize=12)
-    if selecao == "Porcentagem (%)":
-        ax.set_ylim(0, 100)
+    return quantidade, porcentagem
+
+
+def df_quantidade_nucleotideos(seq1, seq2):   
+
+    df = pd.DataFrame(columns=["Feature", "Sequência 1", "Sequência 2"])
   
-    return df, fig
+    quantidade1, _ = conta_nucleotideo(seq1)
+    quantidade2, _ = conta_nucleotideo(seq2)
+
+    df["Feature"] = ["Adenina", "Citosina", "Guanina", "Timina", "Total"]
+    df["Sequência 1"] = quantidade1
+    df["Sequência 2"] = quantidade2
+
+    return df
 
 
-def conteudo_gc(sequencia):
-    conta = Counter(sequencia) 
-    total = len(sequencia)
-    gc = conta["G"] + conta["C"]
-    
-    porc_gc = calcula_porcentagem(gc, total)
-    porc_at = 100 - porc_gc
-     
-    df = pd.DataFrame({"Porcentagem (%)": [porc_gc, porc_at]},
-                        index=["GC", "AT"])
-    
+def df_porcentagem_nucleotideos(seq1, seq2):   
+
+    df = pd.DataFrame(columns=["Feature", "Sequência 1", "Sequência 2"])
+  
+    _, porcentagem1 = conta_nucleotideo(seq1)
+    _, porcentagem2 = conta_nucleotideo(seq2)
+
+    df["Feature"] = ["Adenina", "Citosina", "Guanina", "Timina", "Total"]
+    df["Sequência 1"] = porcentagem1
+    df["Sequência 2"] = porcentagem2
+
+    return df
+
+
+def gera_grafico(df, selecao="Quantidade"):
+
+    df_melt = pd.melt(df[:4], id_vars="Feature", var_name="Sequência", value_name=selecao)
+
     fig, ax = plt.subplots(figsize=(6,3))
-    ax = sns.barplot(x=df.index, y="Porcentagem (%)", data=df)
+    ax = sns.barplot(x='Feature', y=selecao, hue='Sequência', data=df_melt)
+    ax.set_title(f"{selecao} das bases", fontweight="bold", fontsize=14)
+    ax.set_xlabel("Base", fontsize=12)
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.legend(bbox_to_anchor=(1.35, 1))
+  
+    if selecao == "Porcentagem":
+        ax.set_ylim(0, 100)
+        ax.set_ylabel("Porcentagem (%)", fontsize=12)    
+    else:
+        ax.set_ylabel("Quantidade", fontsize=12) 
+
+    return fig
+
+
+def conteudo_gc(seq1, seq2):
+       
+    df = pd.DataFrame(columns=["Feature", "Sequência", "Porcentagem"])
+
+    _, porcentagem1 = conta_nucleotideo(seq1)
+    _, porcentagem2 = conta_nucleotideo(seq2)
+
+    gc1 = porcentagem1[2] + porcentagem1[1]
+    gc2 = porcentagem2[2] + porcentagem2[1]
+
+    df["Feature"] = ["GC", "GC"]
+    df["Sequência"] = ["Sequência 1", "Sequência 2"]
+    df["Porcentagem"] = [gc1, gc2]
+  
+    return df
+
+
+def gera_grafico_gc(df):
+ 
+    fig, ax = plt.subplots(figsize=(6,3))
+    ax = sns.barplot(x="Feature", y="Porcentagem", hue="Sequência", data=df)
     ax.set_title(f"Conteúdo GC", fontweight="bold", fontsize=14)
     ax.set_ylabel("Porcentagem (%)", fontsize=12)
     ax.set_ylim(0, 100)
+    ax.set_xlabel(None)
+    plt.legend(bbox_to_anchor=(1.35, 1))
+
+    return fig
+
+
+def valor_maximo(c1, c2, lado, cima, diagonal):
+    if (c1 == c2) and ((diagonal+1) >= lado) and ((diagonal+1) >= cima):
+        diagonal += 1
+        return diagonal
+  
+    elif (lado >= cima) and (lado >= diagonal):
+        return lado
+  
+    else:
+        return cima
+
+
+def acha_caminho(c1, c2, lado, cima, diagonal):
+    if (c1 == c2) and ((diagonal+1) >= lado) and ((diagonal+1) >= cima):
+        return "\\"
+  
+    elif (lado >= cima) and (lado >= diagonal):
+        return "-"
+  
+    else:
+        return "|"
+
+
+def lcs(seq1, seq2):
+  pontuacao = []
+  caminho = []
+
+  # preencher a matriz
+  for i in range(0, len(seq1)):
+    pontuacao.append([0] * len(seq2))
+    caminho.append([""] * len(seq2))
+
+  # preencher a primeira linha com "-" e a primeira coluna com "|"
+  for i in range(0, len(seq1)):
+    caminho[i][0] = "|"
+  for j in range(0, len(seq2)):
+    caminho[0][j] = "-"
+
+  # linha
+  for i in range(1, len(seq1)):
+    # coluna
+    for j in range(1, len(seq2)):
+      # devolver a valor maximo do L
+      pontuacao[i][j] = valor_maximo(seq2[j], seq1[i], pontuacao[i][j-1], pontuacao[i-1][j], pontuacao[i-1][j-1])
+      caminho[i][j] = acha_caminho(seq2[j], seq1[i], pontuacao[i][j-1], pontuacao[i-1][j], pontuacao[i-1][j-1])
+  
+  return pontuacao, caminho
+
+
+def gera_alinhamento(seq1, seq2, pontuacao, caminho):
+    ali_seq1 = ""
+    ali_seq2 = ""
+
+    # linhas
+    i = len(seq1)-1
+    # colunas
+    j = len(seq2)-1
+
+    while (i != 0) or (j != 0):
+        if caminho[i][j] == "\\":
+            ali_seq1 = seq1[i] + ali_seq1
+            ali_seq2 = seq2[j] + ali_seq2
+            i -= 1
+            j -= 1
     
-    return df, fig
+        elif caminho[i][j] == "-":
+            ali_seq1 = "-" + ali_seq1
+            ali_seq2 = seq2[j] + ali_seq2
+            j -= 1
+        
+        else:
+            ali_seq1 = seq1[i] + ali_seq1
+            ali_seq2 = "-" + ali_seq2
+            i -= 1
+
+    matches = pontuacao[-1][-1]
+
+    return matches, ali_seq1, ali_seq2  
 
 
 def main():
 
     imagem = Image.open("dna-logo.jpg")
-    default_input = open("sars_cov_2.fasta", "r").read()
+    default_input1 = "ATCAACGGGATCGTAAAGCAAGATTCCGACTATCGTAGCTAGCTTGGAAAA"
+    default_input2 = "ATCAATCGATCGTAAAGCAGATTCCGACTAAAGTAGCTAGCTTGTAAAT"
     help_text = "Insira uma sequência de nucleotídeos ou uma sequência no formato FASTA"
 
     st.image(imagem, use_column_width=True)
+    st.title("Alinhamento global de sequências de DNA")
+    st.markdown("""<p style='text-align: justify'>
+    WebApp que faz o alinhamento global de sequências de DNA, par a par, usando o algoritmo de 
+    <b>Needleman-Wunsch</b>, um dos algoritmos mais utilizados para o alinhamento de sequências biológicas. 
+    Para o alinhamento, foi considerado apenas os <b>matches</b> entre as bases das sequências. 
+    Esse WebApp também analisa a quantidade e a porcentagem de <b>A C G T</b> e o <b>Conteúdo GC</b> 
+    de cada sequência.
+    </p>""", unsafe_allow_html=True)
 
-    st.title("Contando nucleotídeos no DNA")
-    st.markdown("""
-    Web App que conta a quantidade e porcentagem de **A C G T** e o **Conteúdo GC** de uma sequência de **DNA**.
-    """)
+    st.subheader("**Insira as suas sequências de DNA:**")       
+    seq1 = st.text_area(label="Insira abaixo a primeira sequência e pressione Ctrl + ENTER", 
+                        value=default_input1, height=200, help=help_text)
 
-    st.subheader("**Insira a sua sequência de DNA**")       
-    sequencia = st.text_area(label="Insira abaixo a sua sequência e pressione Ctrl + ENTER", 
-                            value=default_input, height=250, help=help_text)
+    seq2 = st.text_area(label="Insira abaixo a segunda sequência e pressione Ctrl + ENTER", 
+                        value=default_input2, height=200, help=help_text)
 
-    if sequencia:
-        sequencia_formatada = formata_sequencia(sequencia)
+    if seq1 and seq2:
+
+        seq1_formatada = formata_sequencia(seq1)
+        seq2_formatada = formata_sequencia(seq2)
         
-        if eh_dna(sequencia_formatada):
-  
-            st.subheader("**# 1. Análise dos nucleotídeos:**")
-            selecao = st.selectbox("Selecione o gráfico:", ["Quantidade", "Porcentagem (%)"])
-            df_nucleotideo, grafico_nucleotideo = conta_nucleotideos(sequencia_formatada, selecao) 
-            st.dataframe(df_nucleotideo)
-            st.pyplot(grafico_nucleotideo)
+        if eh_dna(seq1_formatada) and eh_dna(seq2_formatada) :
 
-            st.subheader("**# 2. Conteúdo GC:**")
-            df_gc, grafico_gc = conteudo_gc(sequencia_formatada) 
+            matriz_pontuacao, matriz_caminho = lcs(seq1_formatada, seq2_formatada)
+            matches, ali_seq1, ali_seq2 = gera_alinhamento(seq1_formatada, seq2_formatada, matriz_pontuacao, matriz_caminho)
+            
+            df_quantidade = df_quantidade_nucleotideos(seq1_formatada, seq2_formatada)
+            df_porcentagem = df_porcentagem_nucleotideos(seq1_formatada, seq2_formatada) 
+            
+            df_gc = conteudo_gc(seq1_formatada, seq2_formatada)
+            grafico_gc = gera_grafico_gc(df_gc)
+
+            
+            st.subheader("**# 1. Alinhamento global:**")
+            st.write("Total de matches:", matches)
+            st.write("(1)", ali_seq1)
+            st.write("(2)", ali_seq2)
+         
+            st.subheader("**# 2. Análise dos nucleotídeos:**")
+            selecao = st.selectbox("Selecione o gráfico:", ["Quantidade", "Porcentagem"])
+     
+            if selecao == "Quantidade":
+                df  = df_quantidade_nucleotideos(seq1_formatada, seq2_formatada)
+                grafico = gera_grafico(df_quantidade, "Quantidade")
+                st.dataframe(df)
+                st.pyplot(grafico)
+
+            elif selecao =="Porcentagem":
+                df = df_porcentagem_nucleotideos(seq1_formatada, seq2_formatada)
+                grafico = gera_grafico(df_porcentagem, "Porcentagem")
+                st.dataframe(df)
+                st.pyplot(grafico)
+                  
+            st.subheader("**# 3. Conteúdo GC:**")
             st.dataframe(df_gc)
             st.pyplot(grafico_gc)
 
         else:
-            st.write("*A sequência inserida não é um DNA =(*")
-            st.write("*Insira uma sequência de DNA*")
+            st.write("*As sequências inseridas não são DNA =(*")
+            st.write("*Insira nvamente suas sequências*")        
 
 
 if __name__ == "__main__":
